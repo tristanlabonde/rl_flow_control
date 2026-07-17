2dc222797bcb6d76c5ab298728ebd48c8e432b6c
Tristan Collemiche <collemichet@Tristans-MacBook-Pro.local>
2026-07-17 15:24:49 +0900

    premiere version entierement fonctionnelle

--- flow_control.py
+++ flow_control.py
@@ -2,7 +2,8 @@ import subprocess
 import numpy as np
 import torch
 import time
-from rl_fc_model import RL_FlowControl_Agent, nb_snapshots, nb_components, ng
+import os
+from rl_fc_model import RL_FlowControl_Agent, nb_snapshots, nb_components, ng, starting_x, control_width, control_length, cutting_rate
 
 def parse_results(verbose=True): #results are stored in data/
     # Read the results of the simulation from the output file stats1d.out and return a 4 x height array where row 0 is the height, row 1 is the u-velocity variance, row 2 is the v-velocity variance and row 3 is the w-velocity variance.
@@ -36,30 +37,37 @@ def compute_tke(variances, verbose=True):
     return tke
 
 def parse_input(filename, verbose=True):
-    # Placeholder for actual parsing logic
     if verbose:
         print(f"Parsing input file {filename}...")
-    with open(filename, 'r') as f:
-        data = f.read()
-    velocity_field = np.zeros((nb_snapshots, nb_components, ng[0], ng[1], ng[2]))
-    for t in range(nb_snapshots):
-        for c in range(nb_components):
-            for i in range(ng[0]):
-                for j in range(ng[1]):
-                    for k in range(ng[2]):
-                        velocity_field[t][c][i][j][k] = (t+1)*10000+(c+1)*1000+i*100+j*10+k
+    
+    data = np.loadtxt(filename)
+    means = np.array([data[:, 0], data[:, 1], data[:, 2], data[:, 3]]) # position z, means of u, v, w on the x-y corresponding planes
+
+    velocity_field = np.zeros((nb_components, ng[0], ng[1], ng[2]))
+    for z in range(ng[2]):
+        velocity_field[0, :, :, z] = means[1][z]
+    velocity_field[2, :, :, ng[2]-1] = means[3][ng[2]-1]
     return velocity_field
 
-def create_input(filename, train_velocity_field, verbose=True):
-    # Placeholder for input creation logic based on loss
+def create_input(filename, wall_blowing_amps, verbose=True):
     if verbose:
         print(f"\tCreating input file {filename}")
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w') as f:
+        for t in range(nb_snapshots):
+            for x in range(control_width):
+                for y in range(control_length):
+                    for i in range(cutting_rate):
+                        for j in range(cutting_rate):
+                            f.write(f"{t} {starting_x + i} {y + j} {wall_blowing_amps[t, x*control_width + y]}\n")
+                    
+    copy_command = ["cp", filename, "./wall_blowing_input.txt"]
+    subprocess.run(copy_command, check=True)
+    
 
-def launch_simulation(filename, verbose=True):
-    # Launch the simulation using the command sbatch --wait srun.sh filename, passing the velocity field input filename as an argument.
-    command = ["sbatch", "--wait", "srun.sh", filename]  # velocity field as an argument
-    command = ["sbatch", "--wait", "srun.sh"] # test
-    # command = ["ls"]
+def launch_simulation(verbose=True):
+    # Launch the simulation using the command sbatch --wait srun.sh.
+    command = ["sbatch", "--wait", "srun.sh"]
     reset_command = ["rm", "-rf", "data/*"]
 
     if verbose:
@@ -84,11 +92,11 @@ def launch_simulation(filename, verbose=True):
 def criterion_tke(train_filename, train_preds, verbose=True):
     # Create the input file for the simulation using the prediction done by the rl agent, launch the simulation, parse the results, and compute the TKE.
     create_input(train_filename, train_preds, verbose)
-    launch_simulation(train_filename, verbose)
+    launch_simulation(verbose)
     res = parse_results(verbose)
     return compute_tke(res, verbose)
 
-def train(model, input_tensor, nb_epoch, optimizer, criterion, scheduler, verbose=True):
+def train(model, input_velocity_tensor, input_time_tensor, nb_epoch, optimizer, criterion, scheduler, verbose=True):
     # Train the RL agent for a specified number of epochs, using the provided optimizer, criterion, and scheduler.
     print("Training starts...\n")
     
@@ -101,15 +109,14 @@ def train(model, input_tensor, nb_epoch, optimizer, criterion, scheduler, verbos
         #valid_loss = 0.0
 
         model.train()
-        x = input_tensor
         optimizer.zero_grad()
-        action_mean = model(x)
+        action_mean = model(input_velocity_tensor, input_time_tensor)
         distribution = torch.distributions.Normal(action_mean, exploration_noise)
         action_sample = distribution.sample()
         log_prob = distribution.log_prob(action_sample).sum()
         action_np = action_sample.cpu().numpy()
         action_np = np.clip(action_np, -1.0, 1.0)
-        train_filename = f"training_input_{epoch + 1}.txt"
+        train_filename = f"trainings_inputs/training_input_{epoch + 1}.txt"
         reward = criterion(train_filename, action_np, verbose)
         loss = -log_prob * reward
         loss.backward()
@@ -129,6 +136,7 @@ def train(model, input_tensor, nb_epoch, optimizer, criterion, scheduler, verbos
 def init_train(filename, nb_epoch, verbose=True):
     # Initialize the training process by parsing the input velocity field, setting up the device (GPU or CPU), creating the model, optimizer, criterion, and scheduler, and then calling the train function.
     train_velocity_field = parse_input(filename, verbose)
+    time_steps = torch.linspace(0.0, 1.0, nb_snapshots).unsqueeze(1)
 
     if torch.cuda.is_available():
         print(f"GPU found : {torch.cuda.get_device_name(0)}")
@@ -140,7 +148,8 @@ def init_train(filename, nb_epoch, verbose=True):
         print("GPU not found, using CPU.")
         device = torch.device("cpu")
 
-    input_tensor = torch.from_numpy(train_velocity_field).float().to(device)
+    input_velocity_tensor = torch.from_numpy(np.array([train_velocity_field])).float().to(device)
+    input_time_tensor = time_steps.to(device)
     model = RL_FlowControl_Agent().to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -148,6 +157,6 @@ def init_train(filename, nb_epoch, verbose=True):
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',factor=0.5,patience=2)
 
     start = time.time()
-    history = train(model, input_tensor, nb_epoch, optimizer, criterion, scheduler)
+    history = train(model, input_velocity_tensor, input_time_tensor, nb_epoch, optimizer, criterion, scheduler)
     end = time.time()
     print(f"Training took {end-start:.3f}s")
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import flow_control
 
 def main():
     argc = len(sys.argv)
-    assert argc >= 2, "Usage: python main.py [--verbose] <input_filename> <nb_epoch>(optional)"
-    verbose = sys.argv[1] == "--verbose"
+    assert argc >= 2, "Usage: python main.py [-v/--verbose] <input_filename> <nb_epoch>(optional)"
+    verbose = sys.argv[1] == "--verbose" or sys.argv[1] == "-v"
     filename = sys.argv[1 + int(verbose)]
     if argc >= 3 + int(verbose):
         nb_epoch = int(sys.argv[2 + int(verbose)])
