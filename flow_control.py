6e9510bd692ab77e2fe3e6ca89768154640fae7b
Tristan Collemiche <collemichet@Tristans-MacBook-Pro.local>
2026-07-29 11:44:23 +0900

    verifier que x.bin existe est une mauvaise idee car il est creer par write_xdmf.py, a la place on va verifier que stdout.jobid fini bien par *** Fim ***

--- flow_control.py
+++ flow_control.py
@@ -4,15 +4,17 @@ import torch
 import time
 import os
 import rl_fc_models as model
+from collections import deque
 
-def parse_results(verbose=True): #results are stored in data/
+def parse_results(jobid, verbose=True): #results are stored in data/
     # Read the results of the simulation from the output file stats1d.out and return a 4 x height array where row 0 is the height, row 1 is the u-velocity variance, row 2 is the v-velocity variance and row 3 is the w-velocity variance.
     if verbose:
         print("\tParsing results...")
 
-    filepath = os.path.join("data", "x_bin")
-    if not os.path.exists(filepath):
-        return None
+    with open('jobs/stdout.' + jobid, 'r', encoding='utf-8') as f:
+        derniere_ligne = deque(f, maxlen=1)[0]
+        if derniere_ligne.strip() != "*** Fim ***":
+            return None
     
     results_file = "data/stats1d.out"
     
