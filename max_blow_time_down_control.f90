711a0c20eaca248b539f53f56bf3b2c29d680bc9
Tristan Collemiche <collemichet@Tristans-MacBook-Pro.local>
2026-07-29 16:31:48 +0900

    tests de descente de force de blow de 0.1 tout les certains nombres de istep (1000, 500, 300) et test de max_blow negatifsA

--- max_blow_time_down_control.f90
+++ max_blow_time_down_control.f90
@@ -8,7 +8,8 @@ subroutine apply_wall_blowing(n, dl, istep, w, lo)
   real(rp), intent(in) :: dl(3)
   real(rp), intent(inout) :: w(0:,0:,0:)
 
-  real(rp), parameter :: max_blow = -0.8_rp ! Maximum blowing force
+  real(rp), parameter :: max_blow = 0.9_rp ! Maximum blowing force
+  integer :: freq = 300
 
   integer :: x, y, gx, gy
 
@@ -17,12 +18,15 @@ subroutine apply_wall_blowing(n, dl, istep, w, lo)
     do x = 1, n(1)
       gx = lo(1) - 1 + x ! Indice x global
       if (gx >= 101 .and. gx <= 164 .and. gy >= 1 .and. gy <= 128) then
-        w(x, y, 0) = max_blow
+        w(x, y, 0) = max_blow - (istep/freq)*0.1
         w(x, y, 1) = w(x, y, 0)
       end if
-
     end do
   end do
+
+  ! if (mod(istep, 1000) == 0) then
+  !   print *, "istep", istep, "w", max_blow - (istep/1000)*0.1
+  ! endif
 
   !$acc update device(w(:,:,0:1))
 
