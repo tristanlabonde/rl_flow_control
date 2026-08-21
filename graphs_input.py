e117b7a5be357d76b866fc76bf7b05f4e095191d
Tristan Collemiche <collemichet@Tristans-MacBook-Pro.local>
2026-08-21 17:50:32 +0900

    graphs_input.py n'affiche plus la figure hand made

--- graphs_input.py
+++ graphs_input.py
@@ -8,36 +8,36 @@ control_length = 128//cutting_rate
 
 nb_coeffs = 1
 
-########################## Hand made W distribution
-x_grid = starting_x + np.arange(0.0, control_width, 1)*cutting_rate
-y_grid = np.arange(0.0, control_length, 1)*cutting_rate
-k_modes = np.arange(1, nb_coeffs + 1, dtype=np.float32)
-sin_x = np.sin(2*np.pi/(control_width * cutting_rate) * k_modes[:, None] * x_grid[None, :])
-sin_y = np.sin(2*np.pi/(control_length * cutting_rate) * k_modes[:, None] * y_grid[None, :])
-
-a = [[np.random.randn() for _ in range(nb_coeffs)]]
-c = [-0.0623]
-
-W = np.zeros((control_width, control_length))
-for i in range(nb_coeffs):
-    W += c[i] * sin_x[i][:, None] * sin_y[i][None, :]
-
 x = np.arange(starting_x, starting_x + control_width, 1)
 y = np.arange(0, control_length, 1)
 X, Y = np.meshgrid(x, y)
 
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111, projection='3d')
+########################## Hand made W distribution
+# x_grid = starting_x + np.arange(0.0, control_width, 1)*cutting_rate
+# y_grid = np.arange(0.0, control_length, 1)*cutting_rate
+# k_modes = np.arange(1, nb_coeffs + 1, dtype=np.float32)
+# sin_x = np.sin(2*np.pi/(control_width * cutting_rate) * k_modes[:, None] * x_grid[None, :])
+# sin_y = np.sin(2*np.pi/(control_length * cutting_rate) * k_modes[:, None] * y_grid[None, :])
 
-surface = ax.plot_surface(X, Y, W.T, cmap='viridis', edgecolor='none')
+# a = [[np.random.randn() for _ in range(nb_coeffs)]]
+# c = [-0.0623]
 
-ax.set_title("Blowing Velocity W Distribution Wanted, Handmade")
-ax.set_xlabel("Position X")
-ax.set_ylabel("Position Y")
-ax.set_zlabel("Velocity W")
-fig.colorbar(surface, ax=ax, shrink=0.5, aspect=5)
+# W = np.zeros((control_width, control_length))
+# for i in range(nb_coeffs):
+#     W += c[i] * sin_x[i][:, None] * sin_y[i][None, :]
 
-plt.show()
+# fig = plt.figure(figsize=(8, 6))
+# ax = fig.add_subplot(111, projection='3d')
+
+# surface = ax.plot_surface(X, Y, W.T, cmap='viridis', edgecolor='none')
+
+# ax.set_title("Blowing Velocity W Distribution Wanted, Handmade")
+# ax.set_xlabel("Position X")
+# ax.set_ylabel("Position Y")
+# ax.set_zlabel("Velocity W")
+# fig.colorbar(surface, ax=ax, shrink=0.5, aspect=5)
+
+# plt.show()
 
 ########################## W distribution by RL model
 
