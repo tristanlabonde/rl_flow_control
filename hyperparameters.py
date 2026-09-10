83014cfeec72c6495d9a2d175b886326739b1d8f
Tristan Collemiche <collemichet@Tristans-MacBook-Pro.local>
2026-09-10 17:33:21 +0900

    correction sur la grosse boucle hyperparametre car trop long, desormais : max_blow de 0.3 a 0.8 tout les 0.1, nb_coeffs de 1 a 4 tout les 1, devrais terminer mardi vers 17h11

--- hyperparameters.py
+++ hyperparameters.py
@@ -6,10 +6,11 @@ cutting_rate = 1
 control_width = 64//cutting_rate
 control_length = ng[1]//cutting_rate
 nb_actions = control_width * control_length
-nb_coeffs = 1 # number of Fourier series coefficients to predict
+
+nb_coeffs = 4 # number of Fourier series coefficients to predict
 
 tke_ref = 0
 gamma = 0.3
 nb_epoch = 50
 max_blow = 0.8
-blowstep = 0.05
+blowstep = 0.1
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import subprocess
+import os
+import glob
 import sys
 import flow_control
 import hyperparameters as hp
@@ -15,17 +16,18 @@ def main():
 
     max_blow = hp.max_blow
     nb_coeffs = hp.nb_coeffs
-    for blow in range(1, int(max_blow/hp.blowstep) + 1):
-        blow = blow * hp.blowstep
+    max_blow_range = int(max_blow/hp.blowstep)
+    for blow in range(3, max_blow_range + 1):
+        blow = round(blow * hp.blowstep, 2)
         for nb in range(1, nb_coeffs + 1):
-            reset_command = ["rm", "-rf", "./jobs/*"]
-            subprocess.run(reset_command, check=True)
+            if os.path.exists("./jobs"):
+                for item in glob.glob("./jobs/*"):
+                    os.remove(item)        
             hp.nb_coeffs = nb
             hp.max_blow = blow
             print(f"Training with max_blow = {blow}, nb_coeffs = {nb}")
             flow_control.init_train(filename, nb_epoch, verbose)
-            copy_stdout_command = ["mv", "./jobs/stdout_control.*", "./saved_stdout/stdout_control_blow_" + str(blow) + "_nb_" + str(nb)]
-            subprocess.run(copy_stdout_command, check=True)
+
 
 if __name__ == "__main__":
     main()
